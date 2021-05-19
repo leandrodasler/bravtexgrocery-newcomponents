c63cadb4a8c1993602e6fb442377eacbd940a79c
lucasgralcantara <lucas.alcantara@cubos.io>
2021-05-19 10:19:29 -0300

    feat: add SellerText component

---
+++ react/components/SellerText/SellerText.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./SellerText.css";
+import { useCssHandles } from "vtex.css-handles";
+
+const CSS_HANDLES = [
+  "addressComponent",
+  "pinIcon",
+  "addressComponentInfo",
+  "addressComponentTitle",
+  "currentAddress",
+];
+
+const SellerText = () => {
+  const [seller, setSeller] = React.useState("");
+  const handles = useCssHandles(CSS_HANDLES);
+
+  React.useEffect(() => {
+    fetch(`/api/catalog_system/pvt/seller/list`, {
+      credentials: "same-origin",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setSeller(data[0].Name);
+      });
+  }, []);
+
+  return (
+    <div className={`${handles.sellerDiv}`}>
+		{seller ? <p className={`${handles.sellerText}`}>Vendido e entregue por: {seller}</p> : ''}
+    </div>
+  );
+};
+
+export default SellerText;

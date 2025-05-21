71f8bf7a5f07306de0cec5d42f48c3627d6156b1
Tiago de Andrade Freire <tiago.freire@cubos.io>
2025-05-21 19:09:12 -0300

    fix: consider empty sellers in SellerText component

--- react/components/SellerText/SellerText.tsx
+++ react/components/SellerText/SellerText.tsx
@@ -1,24 +1,24 @@
 import React from 'react'
-import './SellerText.css'
 import { useCssHandles } from 'vtex.css-handles'
 import { useProduct } from 'vtex.product-context'
+import './SellerText.css'
 
 const CSS_HANDLES = ['sellerDiv', 'sellerText']
 
 const SellerText = () => {
   const handles = useCssHandles(CSS_HANDLES)
   const productContextValue = useProduct()
+  const sellers = productContextValue?.selectedItem?.sellers
+  const sellerDefault =
+    sellers?.find(seller => seller.sellerDefault) ?? sellers?.[0]
+
+  if (!sellerDefault) return null
 
   return (
     <div className={`${handles.sellerDiv}`}>
       {productContextValue ? (
         <p className={`${handles.sellerText}`}>
-          Vendido e entregue por:{' '}
-          {
-            productContextValue?.selectedItem?.sellers.find(
-              seller => seller.sellerDefault
-            )?.sellerName
-          }
+          Vendido e entregue por: {sellerDefault.sellerName}
         </p>
       ) : (
         ''
